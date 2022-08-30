1367906d55ac717e82302ab8811dd5bd9c3c2022
Camilo Guerra <guerracamilo92@gmail.com>
2022-08-29 21:45:56 -0300

    agregado de navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" id='nombreNav' href="#">Nombre</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Categorías</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Más Info</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+    </ul>
+  </div>
+</nav>
   )
 }
 
