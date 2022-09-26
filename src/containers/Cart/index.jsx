6afba86e88024c6b7e8ce0b274fa3e575e67c155
Camilo Guerra <guerracamilo92@gmail.com>
2022-09-26 20:02:13 -0300

    cart view

--- src/containers/Cart/index.jsx
+++ src/containers/Cart/index.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { useContext } from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import { Shop } from '../../context/ShopProvider';
+
 
 const Cart = () => {
+
+  const {cart, removeItem, clearCart} = useContext(Shop);
+
+  
+
+const columns = [
+  { field: 'image', headerName: 'Image', width: 400 },
+  { field: 'title', headerName: 'Product', width: 250 },
+  { field: 'quantity', headerName: 'Quantity', width: 130 },
+  {
+    field: 'remove',
+    headerName: '',
+    width: 120,
+  }
+];
+
+const filas = []
+cart.forEach(item => {
+  filas.push({
+    id: item.id,
+    image: item.image,
+    title: item.title,
+    quantity: item.quantity,
+    remove: <button onClick={removeItem}>borrar</button>,
+  })
+
+
+})
+
   return (
-    <div>Cart</div>
-  )
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={filas}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
